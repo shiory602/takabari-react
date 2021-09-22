cf1464f877dab76dedbf098aff3ac7ba0f972f11
shiory602 <suzushioufo@gmail.com>
2021-09-22 16:24:28 -0700

    add 404 page

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopPage from '../pages/TopPage';
 import Header from '../components/Header';
 
+const NotFound = () => {
+  return (
+    <h2>404 NOT FOUND</h2>
+  )
+}
+
 function AppRouter() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
-          path='/'
-          component={Header}
-          exact={true}
-        />
+        <Route path='/' component={Header} exact={true} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
