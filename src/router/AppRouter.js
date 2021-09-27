67c4a02875d89c9ce1117113ddb1cb2df18dd4f3
shiory602 <suzushioufo@gmail.com>
2021-09-27 16:35:42 -0700

    comment out a line

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import TopPage from '../pages/TopPage';
+// import TopPage from '../pages/TopPage';
 
 function AppRouter() {
   return (
